abc3e329a53faacfd14ab0528e82e63c2ae69985
Pete Farris <petefarris@petefarris.me>
2015-07-23 08:14:16 -0500

    Adjusted click selector

--- js/sp-search-scripts.js
+++ js/sp-search-scripts.js
@@ -32,19 +32,32 @@
             $this.append('<div id="cal"><div class="header"></div><table id="days"></table><div id="cal-frame"></div></div>').find(".header").html( '<span class="left button" id="prev"> &lang; </span><span class="month-year" id="label"> June 2010 </span><span class="right button" id="next"> &rang; </span>'); 
             $this.find("#days").html( '<tr> <td>sun</td> <td>mon</td> <td>tue</td> <td>wed</td> <td>thu</td> <td>fri</td> <td>sat</td> </tr>')
             $this.find("#cal-frame").html( '<table class="curr"><tr><td class="nil"></td><td class="nil"></td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td></tr><tr><td>6</td><td>7</td><td>8</td><td>9</td><td>10</td><td class="today">11</td><td>12</td></tr><tr><td>13</td><td>14</td><td>15</td><td>16</td><td>17</td><td>18</td><td>19</td></tr><tr><td>20</td><td>21</td><td>22</td><td>23</td><td>24</td><td>25</td><td>26</td></tr><tr><td>27</td><td>28</td><td>29</td><td>30</td><td class="nil"></td><td class="nil"></td><td class="nil"></td></tr></table>'); 
+            //bind events to calendar
+            
+            // 
+            
+            
          } 
-
+        //switch months function::  
+        
+        //create cal function :: 
+        
+        
+        
         //return calendars:: 
         console.log($this); 
         return this.each( function() {
             //call the init function:: 
-            init(); 
+            init();
+            //set selector(cal1/cal2); 
+            //
             var selector = $this.selector
             //show the calendar on click
                 $(o.position).focus( function(){ 
                     console.log(o.position + ' was focused on'); 
                     $(selector).show();
-                    $(selector).on('click', 'td', function(){ 
+                    //Call the clicked method to store date info here:: 
+                    $(selector + ' #cal-frame').on('click', 'td', function(){ 
                         console.log( $this.selector + 'date clicked'); 
                         //something happens to capture the date parameters and returns a value:: 
                         $this.hide(); 
